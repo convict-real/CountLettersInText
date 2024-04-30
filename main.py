d3d029195c14ab7cfddc2628ece7baff4ae1f8bf
swamprat <92648744+convict-real@users.noreply.github.com>
2024-04-30 10:25:45 -0500

    Rewrote to put all rendering on matplotlib

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 input = input("Enter your text: ")
 
 if input[-1] == " ":
@@ -20,14 +22,29 @@ def main():
     highestCount = max(occurrences, key = occurrences.get)
     lowestCount = min(filtered_occurrences, key = filtered_occurrences.get)
 
-    print(
-        f"Characters: {len(input)}, Words: {len(input.split())}\n"
-        f"Most used: {highestCount}, Least used: {lowestCount}"
-    )
+    sorted_occurrences = sorted(occurrences.items(), key=lambda x: x[1], reverse=True)
+    occurrencesList = []
 
-    for char, count in occurrences.items():
-        print(f"{char}: {count}")
+    for i in range(len(sorted_occurrences)):
+        if i % 2 == 0:
+            occurrencesList.append(sorted_occurrences[i])
+        else:
+            occurrencesList.insert(0, sorted_occurrences[i])
+
+    plt.bar(*zip(*occurrencesList))
+    plt.xlabel('Letter')
+    plt.ylabel('Count')
+
+    for char, count in occurrencesList:
+        plt.text(char, count, str(count), ha='center', va='bottom')
+
+    info_text = (f"Characters: {len(input)}, Words: {len(input.split())}\n"
+                 f"Most used: {highestCount}, Least used: {lowestCount}")
+
+    plt.gcf().text(0, 0, info_text, horizontalalignment='left', verticalalignment='bottom', fontsize=10, wrap=True)
+    plt.subplots_adjust(bottom=0.135)    
+
+    plt.show()
 
 if __name__ == "__main__":
     main()
-    
